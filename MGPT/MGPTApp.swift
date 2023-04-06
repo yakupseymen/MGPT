863bc498e421397fd3928bd181029ef964066a18
yakupseymen <81269289+yakupseymen@users.noreply.github.com>
2023-04-06 21:36:08 +0300

    Initial Commit

---
+++ MGPT/MGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  MGPTApp.swift
+//  MGPT
+//
+//  Created by Yakup Seymen on 6.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct MGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
